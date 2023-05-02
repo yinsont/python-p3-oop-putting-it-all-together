f5dff15d2cea0713f9ae2ec5c695a9853eb295b3
Yinson Tso <yinson.tso@gmail.com>
2023-05-02 16:40:23 -0400

    added alternate

--- lib/shoe.py
+++ lib/shoe.py
@@ -35,3 +35,29 @@ class Shoe:
 
     size = property(get_size, set_size)
     pass
+
+
+# #!/usr/bin/env python3
+
+# class Shoe:
+#     def __init__(self, brand):
+#         self.brand = brand
+#         self.color = None
+#         self._size = None
+#         self.material = None
+#         self.condition = None
+    
+#     def get_size(self):
+#         return self._size
+    
+#     def set_size(self, size):
+#         if type(size) == int:
+#             self._size = size
+#         else:
+#             print("size must be an integer")
+
+#     size = property(get_size, set_size)
+
+#     def cobble(self):
+#         self.condition = "New"
+#         print("Your shoe is as good as new!")
