2967e2d56771b5083df218347dfdb5f670d0be84
Yinson Tso <yinson.tso@gmail.com>
2023-05-02 16:38:19 -0400

    done

--- lib/book.py
+++ lib/book.py
@@ -1,4 +1,42 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
+    def __init__(self, title = 'And Then There Were None', author = 'Agatha Christie', page_count = 0):
+        self.title = title
+        self.author = author
+        self.page_count = page_count
+
+    def get_title(self):
+        return self._title
+    
+    def get_author(self):
+        return self._author
+    
+    def get_page_count(self):
+        return self._page_count
+    
+    def set_title(self, title):
+        if (type(title) == str) and (1 <= len(title)):
+            self._title = title
+        return 
+    
+    def set_author(self, author):
+        if (type(author) == str) and (1 <= len(author)):
+            self._author = author
+        else:
+            pass
+
+    def set_page_count(self, page_count):
+        if (type(page_count) == int):
+            self._page_count = page_count
+        else:
+            print('page_count must be an integer')
+        return
+    
+    def turn_page(self):
+        self._page_count += 1
+        print('Flipping the page...wow, you read fast!')
+    
+    title = property(get_title, set_title)
+    author = property(get_author, set_author)
+    page_count = property(get_page_count, set_page_count)
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,37 @@
 #!/usr/bin/env python3
 
 class Shoe:
+    def __init__(self, brand = 'Adidas', color = 'White', size = 0, material = 'Leather', condition = 'Used'):
+        self.brand = brand
+        self.color = color
+        self.size = size
+        self.material = material
+        self.condition = condition
+
+    def get_brand(self):
+        return self._brand
+    
+    def get_color(self):
+        return self._color
+    
+    def get_size(self):
+        return self._size
+    
+    def get_material(self):
+        return self._material
+    
+    def get_condition(self):
+        return self._condition
+    
+    def cobble(self):
+        self.condition = 'New'
+        print('Your shoe is as good as new!')
+
+    def set_size(self, size):
+        if type(size) == int:
+            self._size = size
+        else:
+            print("size must be an integer")
+
+    size = property(get_size, set_size)
     pass
